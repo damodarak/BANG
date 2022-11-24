e8148f7d0f0afc6c0a673880b09cbf8d61cd4aad
David Kroupa <damodarak@mail.com>
2022-11-24 21:46:42 +0100

    improve macro_du1

--- du1/macro_du1/macro_du1/macro_du1.cpp
+++ du1/macro_du1/macro_du1/macro_du1.cpp
@@ -1,11 +1,96 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
+
+class Macro {
+public:
+    Macro()
+    {
+        inword = inmacro = false;
+        last_space = true;
+        word = "";
+        macro = "";
+    }
+    string next(istream& s)
+    {
+        char c;
+        word = "";
+
+        for (;;) {
+            if (word != "" and !inword and !inmacro)
+            {
+                return word;
+            }
+            c = s.get();
+
+            if (s.fail())
+            {
+                return word;
+            }
+            else
+            {
+                process(c);
+            }
+        }
+    }
+
+//private:
+    bool inword;
+    bool inmacro;
+    bool last_space;
+
+    string word;
+    string macro;
+
+    map<string, string> macra_dict;
+
+    void process(char& c) 
+    {
+        if (inmacro)
+        {
+            if (c == '#')
+            {
+                inmacro = false;
+                process_macro();
+            }
+            else
+            {
+                macro += c;
+            }
+        }
+        else
+        {
+
+        }
+    }
+    void process_macro()
+    {
+        int body = macro.find(" ");
+        macra_dict.insert({ macro.substr(0, body), macro.substr(body + 1, macro.size()-1) });
+        macro = "";
+    }
+};
 
 int main(int argc, char** argv)
 {
     vector<string> arg(argv, argv + argc);
+    Macro m;
+   
+    string str;
 
+    for (;;) {
+        if (cin.fail())
+        {
+            return 0;
+        }
+        else
+        {
+            str = m.next(cin);
+            cout << str;
+        }
+    }
+    
 }
