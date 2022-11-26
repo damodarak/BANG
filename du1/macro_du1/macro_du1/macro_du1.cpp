e1a46b298c833b9c05a05993214f67df49cf7024
David Kroupa <damodarak@mail.com>
2022-11-26 15:17:17 +0100

    finish work du1

--- du1/macro_du1/macro_du1/macro_du1.cpp
+++ du1/macro_du1/macro_du1/macro_du1.cpp
@@ -1,254 +1,207 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <map>
 #include <cctype>
+#include "macro_du1.h"
 
 using namespace std;
 
-class Macro {
-public:
-    Macro()
-    {
-        inword = inmacro = ready = error = macro_last_space = false;
-        last_space = true;
-        word = "";
-        macro = "";
-        next_must = "";
-    }
-    void handlecl(const vector<string>& arg)
-    {
-        string nazev = arg[1];
-        string telo = arg[2];
-        int size = arg.size();
-        for (int i = 3; i < size; i++)
-        {
-            telo += " " + arg[i];
-        }
-        macra_dict.insert({ nazev,telo });
-    }
-    bool fail(istream& s)
-    {
-        return error || s.fail();
-    }
-    string next(istream& s)
-    {
-        char c;
-        word = "";
-
-        for (;;) {
-            if (ready)
-            {
-                ready = false;
-                return word;
-            }
-            c = s.get();
-
-            if (s.fail())
-            {
-                return word;
-            }
-            else
-            {
-                process(c);
-            }
-        }
-    }
-
-private:
-    bool inword;
-    bool inmacro;
-    bool last_space;
-    bool ready;
-    bool error;
-    bool macro_last_space;
-
-    string word;
-    string macro;
-    string next_must;
-
-    map<string, string> macra_dict;
-
-    bool control(char& c)
-    {
-        if ((next_must == "alpha" && !isalpha(c)) || (next_must == "space" && !isspace(c)))
-        {
-            ready = true;
-            error = true;
-            return false;
-        }
-
-        next_must = "";
-        return true;
-    }
-
-    void process(char& c)
-    {
-        if (!control(c))
-        {
-            return;
-        }
-        else if (inmacro)
-        {
-            if (c == '#')
-            {
-                if (macro_last_space)
-                {
-                    inmacro = false;
-                    process_macro();
-                }
-                else
-                {
-                    error = false;
-                    ready = true;
-                }
-            }
-            else
-            {
-                macro_last_space = isspace(c);
-                macro += c;
-            }
-        }
-        else//nejsme v macru
-        {
-            if (c == '#' && last_space)
-            {
-                next_must = "alpha";
-                inmacro = true;
-                ready = true;
-            }
-            else if (inword)
-            {
-                if (isspace(c))
-                {
-                    //check jestli macro
-                    last_space = true;
-                    inword = false;
-                    if (macra_dict.find(word) == macra_dict.end())
-                    {
-                        word += c;
-                        ready = true;
-                    }
-                    else
-                    {
-                        word = macra_dict.find(word)->second + c;
-                        ready = true;
-                    }
-                }
-                else if (isalnum(c))
-                {
-                    word += c;
-                }
-                else//skoncilo slovo...kontrola, jestli existuje makro
-                {
-                    ready = true;
-                    if (macra_dict.find(word) == macra_dict.end())
-                    {
-                        word += c;          
-                    }
-                    else
-                    {
-                        word = macra_dict.find(word)->second + c;
-                    }
-                }
-            }
-            else//nejsme uvnitr slova
-            {
-                word += c;
-                if (last_space)
-                {
-                    if (isspace(c))
-                    {
-                        ready = true;
-                    }
-                    else if(isalpha(c))
-                    {
-                        inword = true;
-                        last_space = false;
-                    }
-                    else
-                    {
-                        last_space = false;
-                    }
-                }
-                else//posledni znak nebyl isspace a nejsme ve slove
-                {
-                    last_space = isspace(c);
-                    ready = isspace(c);
-                }
-            }
-        }
-    }
-    void process_macro()
-    {
-        int telo_start = macro.find(" ");
-        string nazev = macro.substr(0, telo_start);
-        int index = telo_start + 1;
-        string definice = "";
-
-        bool inword = false;
-        string temp = "";
-        int size = macro.size();
-
-        for (;index<size; index++)
-        {
-            if (inword)
-            {
-                if (isalnum(macro[index]))
-                {
-                    temp += macro[index];
-                }
-                else if (macra_dict.find(temp) == macra_dict.end())
-                {
-                    definice += temp + macro[index];
-                    inword = false;
-                    temp = "";
-                }
-                else
-                {
-
-
-                    inword = false;
-                    temp = "";
-                }
-
-            }
-            else
-            {
-
-            }
-        }
-
-
-
-        macra_dict.insert({nazev, definice});
-        macro = "";
-        next_must = "space";
-    }
-};
-
-int main(int argc, char** argv)
+void Macro::handlecl(const vector<string>& arg)
 {
-    Macro m;
-    vector<string> arg(argv, argv + argc);
-    if (arg.size()>1)
+    string nazev = arg[1];
+    string telo = arg[2];
+    int size = arg.size();
+    for (int i = 3; i < size; i++)
     {
-        m.handlecl(arg);
+        telo += " " + arg[i];
     }
-    
-    string str;
+    macra_dict.insert({ nazev,telo });
+}
+
+string Macro::next(istream& s)
+{
+    char c;
+    word = "";
 
     for (;;) {
-        if (m.fail(cin))
+        if (ready)
         {
-            cout << "Error" << endl;
-            return 0;
+            ready = false;
+            return word;
+        }
+
+        c = s.get();
+
+        if (s.fail())
+        {
+            return word;
         }
         else
         {
-            str = m.next(cin);
-            cout << str;
+            process(c);
+        }
+    }
+}
+
+bool Macro::control(char& c)
+{
+    if ((next_must == "alpha" && !isalpha(c)) || (next_must == "space" && !isspace(c)))
+    {
+        ready = true;
+        error = true;
+        return false;
+    }
+
+    next_must = "";
+    return true;
+}
+
+void Macro::process(char& c)
+{
+    if (!control(c))//kontrola spravnosti
+    {
+        return;
+    }
+    else if (inmacro)
+    {
+        if (c == '#')
+        {
+            if (macro_last_space)
+            {
+                inmacro = false;
+                process_macro();
+            }
+            else
+            {
+                error = false;
+                ready = true;
+            }
+        }
+        else
+        {
+            macro_last_space = isspace(c);
+            macro += c;
+        }
+    }
+    else//nejsme v macru
+    {
+        process_notmacro(c);
+    }
+}
+
+void Macro::process_notmacro(char& c)
+{
+    if (c == '#' && last_space)
+    {
+        next_must = "alpha";
+        inmacro = true;
+        ready = true;
+    }
+    else if (inword)
+    {
+        if (isspace(c))
+        {
+            //check jestli macro
+            last_space = true;
+            inword = false;
+            if (macra_dict.find(word) == macra_dict.end())
+            {
+                word += c;
+                ready = true;
+            }
+            else
+            {
+                word = macra_dict.find(word)->second + c;
+                ready = true;
+            }
+        }
+        else if (isalnum(c))
+        {
+            word += c;
+        }
+        else//skoncilo slovo...kontrola, jestli existuje makro
+        {
+            ready = true;
+            if (macra_dict.find(word) == macra_dict.end())
+            {
+                word += c;
+            }
+            else
+            {
+                word = macra_dict.find(word)->second + c;
+            }
+        }
+    }
+    else//nejsme uvnitr slova
+    {
+        word += c;
+        if (last_space)
+        {
+            last_space = isspace(c);
+            if (isspace(c))
+            {
+                ready = true;
+            }
+            else if (isalpha(c))
+            {
+                inword = true;
+            }
+        }
+        else//posledni znak nebyl isspace a nejsme ve slove
+        {
+            last_space = isspace(c);
+            ready = isspace(c);
+        }
+    }
+}
+
+void Macro::process_macro()
+{
+    int telo_start = macro.find(" ");
+    string nazev = macro.substr(0, telo_start);
+    int index = telo_start + 1;
+    string definice = "";
+
+    bool inword = false;
+    string temp = "";
+    int size = macro.size();
+
+    for (;index < size; index++)
+    {
+        if (inword)
+        {
+            if (isalnum(macro[index]))
+            {
+                temp += macro[index];
+            }
+            else if (macra_dict.find(temp) == macra_dict.end())
+            {
+                definice += temp + macro[index];
+                inword = false;
+                temp = "";
+            }
+            else
+            {
+                definice += macra_dict.find(temp)->second + macro[index];
+                inword = false;
+                temp = "";
+            }
+        }
+        else
+        {
+            if (isalnum(macro[index]))
+            {
+                temp += macro[index];
+                inword = true;
+            }
+            else
+            {
+                temp += macro[index];
+                definice += temp;
+                temp = "";
+            }
         }
     }
 
+    definice += temp;
+    macra_dict.insert({ nazev, definice.substr(0, definice.size() - 1) });//nechceme posledni mezeru
+    macro = "";
+    next_must = "space";
 }
--- du1/macro_du1/macro_du1/macro_du1.h
+++ du1/macro_du1/macro_du1/macro_du1.h
@@ -1,8 +1,43 @@
-﻿// macro_du1.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
+﻿#include <map>
+#include <string>
+#include <vector>
 #include <iostream>
 
-// TODO: Reference additional headers your program requires here.
+#ifndef MACRO_DU1_H_
+#define MACRO_DU1_H_
+
+class Macro 
+{
+public:
+    Macro()
+    {
+        inword = inmacro = ready = error = macro_last_space = false;
+        last_space = true;
+        word = "";
+        macro = "";
+        next_must = "";
+    }
+    void handlecl(const std::vector<std::string>& arg);
+    bool fail() {return error;}
+    std::string next(std::istream& s);
+private:
+    bool control(char& c);
+    void process(char& c);
+    void process_notmacro(char& c);
+    void process_macro();
+
+    bool inword;
+    bool inmacro;
+    bool last_space;
+    bool ready;
+    bool error;
+    bool macro_last_space;
+
+    std::string word;
+    std::string macro;
+    std::string next_must;
+
+    std::map<std::string, std::string> macra_dict;
+};
+
+#endif
