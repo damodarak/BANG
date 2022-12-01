545af339fec3544b7d4c7f4e786e628254750085
David Kroupa <damodarak@mail.com>
2022-12-01 18:50:03 +0100

    reset project

--- 8/polymorph/polymorph/polymorph.cpp
+++ 8/polymorph/polymorph/polymorph.cpp
@@ -1,103 +1,20 @@
-﻿#include <iostream>
-#include <memory>
-#include <vector>
-#include <string>
+﻿#include <vector>
+#include "polymorph.h"
 
-using namespace std;
+int main(int argc, char* argv[]) {
+    PolymorficList list;
 
-class AbstractVal {
-public:
-    virtual void print() = 0;
-    /*virtual unique_ptr<AbstractVal> clone() = 0;*/
-    enum T { T_INT, T_STRING };
-    virtual T get_t() = 0;
-};
+    list.add(std::make_unique<IntValue>(50));
+    list.add(std::make_unique<IntValue>(120));
+    list.add(std::make_unique<StringValue>("Hello, there!"));
+    list.print();
 
-using Valptr = unique_ptr<AbstractVal>;
-
-class IntVal : public AbstractVal
-{
-public:
-    IntVal(int x) : x_(x) {};
-    IntVal(const IntVal& tv) : x_(tv.x_) {};
-    virtual T get_t() override
-    {
-        return T_INT;
-    }
-    virtual void print() override
-    {
-        cout << x_ << endl;
-    }
-    /*virtual T get_t() override
-    {
-        return AbstractVal::T_INT;
-    }*/
-   /* virtual Valptr clone() override
-    {
-        return make_unique<IntVal>(*this);
-    }*/
-private:
-    int x_;
-};
-
-class StrVal : public AbstractVal
-{
-public:
-    StrVal(string x) : x_(x) {};
-    virtual T get_t() override
-    {
-        return T_STRING;
-    }
-    virtual void print() override
-    {
-        cout << x_ << endl;
-    }
-    /*virtual T get_t() override
-    {
-        return AbstractVal::T_STRING;
-    }*/
-    /*virtual Valptr clone() override
-    {
-        return make_unique<StrVal>(*this);
-    }*/
-private:
-    string x_;
-};
-
-class Seznam {
-public:
-    void add(Valptr& p)
-    {
-        data.push_back(move(p));
-    }
-    void print()
-    {
-        for (auto&& x : data)
-        {
-            x->print();
-        }
-    }
-    Seznam& operator=(const Seznam& s)
-    {
-       /* for (auto&& x : s.data)
-            data.push_back(make_unique<IntVal>(*x));
-        return *this;*/
+    PolymorficList l1 = list;
 
 
-    }
-private:
-    vector<Valptr> data;
-};
-    
-int main()
-{
-    Seznam s;
-    Valptr vp = make_unique<IntVal>(123);
-    IntVal iv = vp->
-    //IntVal iv = *vp;
-    //Valptr vp1 = make_unique<IntVal>(*vp);
-    //s.add(vp);
-    //s.add(make_unique<StrVal>("456"));
-    s.print();
-    //Seznam s1 = s;
+    ValuePointer vl = std::make_unique<IntValue>(1);
+    //ValuePointer vp1 = std::make_unique<IntValue>(*vl);//error
+    ValuePointer vp2 = vl->clone();
+
+    return 0;
 }
--- 8/polymorph/polymorph/polymorph.h
+++ 8/polymorph/polymorph/polymorph.h
@@ -1,8 +1,85 @@
-﻿// polymorph.h : Include file for standard system include files,
-// or project specific include files.
+﻿#include <iostream>
+#include <vector>
+#include <string>
 
-#pragma once
+#ifndef MFFUK_POLYMORFISM_H
+#define MFFUK_POLYMORFISM_H
 
-#include <iostream>
+class AbstractValue {
+public:
+    virtual void print() = 0;
+    virtual std::unique_ptr<AbstractValue> clone() = 0;
+};
 
-// TODO: Reference additional headers your program requires here.
+using ValuePointer = std::unique_ptr<AbstractValue>;
+
+class IntValue : public AbstractValue {
+public:
+    IntValue(int value) : value_(value) {}
+    IntValue& operator=(const IntValue& iv) {
+        value_ = iv.value_;
+        return *this;
+    }
+
+    virtual ValuePointer clone() override {
+        return std::make_unique<IntValue>(*this);
+    }
+
+    virtual void print() {
+        std::cout << value_ << std::endl;
+    }
+private:
+    int value_;
+};
+
+class StringValue : public AbstractValue {
+public:
+    StringValue(std::string value) : value_(value) {}
+
+    virtual ValuePointer clone() override {
+        return std::make_unique<StringValue>(*this);
+    }
+
+    virtual void print() {
+        std::cout << value_ << std::endl;
+    }
+private:
+    std::string value_;
+};
+
+class PolymorficList {
+public:
+    void add(ValuePointer p) {
+        list_.push_back(std::move(p));
+    };
+
+    PolymorficList() {
+
+    }
+
+    PolymorficList(const PolymorficList& l) {
+        clone(l);
+    }
+
+    PolymorficList& operator=(const PolymorficList& l) {
+        if (this == &l) return *this;
+
+        list_.clear();
+        clone(l);
+        return *this;
+    }
+
+    void print() {
+        for (auto&& value : list_) {
+            value->print();
+        }
+    }
+private:
+    std::vector<ValuePointer> list_;
+
+    void clone(const PolymorficList& l) {
+        for (auto&& x : l.list_) list_.push_back(x->clone());
+    }
+};
+
+#endif
