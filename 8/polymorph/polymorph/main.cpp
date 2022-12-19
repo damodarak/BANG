5b4cf7e6ec2e797167809824fbe013c5a488c081
David Kroupa <damodarak@mail.com>
2022-12-19 14:23:50 +0100

    change 8/polymorph

---
+++ 8/polymorph/polymorph/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <vector>
+#include "polymorph.cpp"
+
+int main(int argc, char* argv[]) {
+    PolymorficList list;
+
+    list.add(std::make_unique<IntValue>(50));
+    list.add(std::make_unique<IntValue>(120));
+    list.add(std::make_unique<StringValue>("Hello, there!"));
+    list.print();
+
+    PolymorficList l1 = list;
+
+
+    ValuePointer vl = std::make_unique<IntValue>(1);
+    //ValuePointer vp1 = std::make_unique<IntValue>(*vl);//error
+    ValuePointer vp2 = vl->clone();
+
+    return 0;
+}
